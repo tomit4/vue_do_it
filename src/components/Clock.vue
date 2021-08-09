b3cb7ab852fc210c239e916239df826f4549ab2e
tomit4 <mosssap@gmail.com>
2021-08-09 08:48:55 -0700

    updated clock component css

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,53 +1,69 @@
 <template>
-  <div id="clock"></div>
+  <div id="clock">
+    {{ time }}
+  </div>
 </template>
 
 <script>
 export default {
-//     setInterval(showTime, 1000);
-//         function showTime() {
-//             let time = new Date();
-//             let hour = time.getHours() - 7; // adjusted because internal clock is off by seven hours... otherwise works fine
-//             let min = time.getMinutes();
-//             let sec = time.getSeconds();
-//             am_pm = "AM";
-  
-//             if (hour > 12) {
-//                 hour -= 12;
-//                 am_pm = "PM";
-//             }
-//             if (hour == 0) {
-//                 hr = 12;
-//                 am_pm = "AM";
-//             }
-  
-//             hour = hour < 10 ? "0" + hour : hour;
-//             min = min < 10 ? "0" + min : min;
-//             sec = sec < 10 ? "0" + sec : sec;
-  
-//             let currentTime = hour + ":" 
-//                 + min + ":" + sec + am_pm;
-  
-//             document.getElementById("clock")
-//                 .innerHTML = currentTime;
-//         }
-  
-//         showTime();
+  data() {
+    return {
+      time: "00:00:00AM"
+    }
+  },
+  methods: {
+    showTime() {
+      let time = new Date()
+      let hour = time.getHours() - 7
+      let min = time.getMinutes()
+      let sec = time.getSeconds()
+      let am_pm = "AM"
+
+      if (hour > 12) {
+        hour -= 12
+        am_pm = "PM"
+      }
+      
+      if (hour == 0) {
+        hour = 12
+        am_pm = "PM"
+      }
+
+      hour = hour < 10 ? "0" + hour : hour
+      min = min < 10 ? "0" + min : min
+      sec = sec < 10 ? "0" + sec : sec
+      
+      let currentTime = hour + ":" + min + ":" + sec + am_pm
+
+      this.time = currentTime
+    },
+  },
+  mounted() {
+    setInterval(this.showTime, 1000)    
+  }
 
 }
 </script>
 
 <style scoped>
+
 #clock {
+    display: block;
     font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
     font-weight: 900;
-    font-size: 25px;
-    width: 250px;
-    margin: 200px;
+    font-size: 20px;
+    justify-content: center;
+    align-content: center;
+    width: 150px;
+    /* the margin auto feature centers our clock no matter the window dimension */
+    margin: 10px auto; 
+    padding: 0;
     text-align: center;
     border: 4px solid #206557ff;
     border-radius: 5px;
     color: #fff;
     background-color: #206557ff;
 }
+
 </style>
